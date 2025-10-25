d657ae05b03263460fb9a61897dca881e9401b83
garecrow <147332812+garecrow@users.noreply.github.com>
2025-10-25 12:59:25 -0700

    Align split view with Libadwaita guidance

---
+++ src/monitor_item.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define MONITOR_TYPE_ITEM (monitor_item_get_type())
+
+G_DECLARE_FINAL_TYPE(MonitorItem, monitor_item, MONITOR, ITEM, GObject)
+
+MonitorItem *monitor_item_new(const gchar *display_id,
+                               const gchar *name,
+                               const gchar *bus,
+                               const gchar *serial,
+                               const gchar *manufacturer,
+                               const gchar *mccs_version,
+                               const gchar *firmware,
+                               const gchar *manufacture_date);
+
+const gchar *monitor_item_get_display_id(MonitorItem *self);
+const gchar *monitor_item_get_name(MonitorItem *self);
+const gchar *monitor_item_get_bus(MonitorItem *self);
+const gchar *monitor_item_get_serial(MonitorItem *self);
+const gchar *monitor_item_get_manufacturer(MonitorItem *self);
+const gchar *monitor_item_get_mccs_version(MonitorItem *self);
+const gchar *monitor_item_get_firmware(MonitorItem *self);
+const gchar *monitor_item_get_manufacture_date(MonitorItem *self);
+
+void monitor_item_set_name(MonitorItem *self, const gchar *name);
+
+G_END_DECLS
